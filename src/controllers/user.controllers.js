7d5d74e47efa2c3ee1a41b92ea38d6f2a28c1743
Anshul20287 <anshul1234556431545464559@gmail.com>
2025-06-09 18:50:37 +0530

    learning about postman

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,7 +2,7 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 import {ApiError} from "../utils/ApiError.js"
 import {User} from "../models/user.models.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
-import { ApiResponse } from "../utils/apiresponse.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
 
 const registerUser = asyncHandler(async (req,res)=>{
     // get users details from frontend
@@ -31,7 +31,7 @@ const registerUser = asyncHandler(async (req,res)=>{
         throw new ApiError(400,"All fields aew required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or:[{username} , {email}]
     })
 
@@ -39,8 +39,13 @@ const registerUser = asyncHandler(async (req,res)=>{
         throw new ApiError(409,"User with email or username already exist")
     }
 
-    const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    const avatarLocalPath = req.files?.avatar?.[0]?.path
+    const coverImageLocalPath = req.files?.coverImage?.[0]?.path
+
+    // let coverImageLocalPath
+    // if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+    //     coverImageLocalPath=req.files.coverImage[0].path
+    // }
 
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar file is required")
--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -1,12 +1,20 @@
-import multer from "multer"
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => cb(null, "uploads/"),
+//   filename: (req, file, cb) => cb(null, file.originalname)
+// });
+
+import multer from "multer";
 
 const storage = multer.diskStorage({
-    destination: function(req,file,cb){
-        cb(null,"./public/temp")
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
     },
-    filename:function(req,file,cb){
-        cb(null,file.originalname)
+    filename: function (req, file, cb) {
+      
+      cb(null, file.originalname)
     }
+  })
+  
+export const upload = multer({ 
+    storage, 
 })
-
-export const upload = multer({storage:storage})
